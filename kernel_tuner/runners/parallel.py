2462ac7ac6a7b21f4fa89d1c14a1c9b7dc8ca756
FinnSchuirink <s4065328@vuw.leidenuniv.nl>
2026-05-09 14:17:17 +0200

    Parallelized the sequential runner

--- kernel_tuner/runners/parallel.py
+++ kernel_tuner/runners/parallel.py
@@ -1,8 +1,9 @@
 """The multithreaded runner for parallel tuning of the parameter space."""
 import logging
+import os
 from datetime import datetime, timezone
 from time import perf_counter
-import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from kernel_tuner.core import DeviceInterface
 from kernel_tuner.runners.runner import Runner
@@ -29,6 +30,14 @@ class ParallelRunner(Runner):
             #detect language and create high-level device interface
             self.dev = DeviceInterface(kernel_source, iterations=iterations, observers=observers, **device_options)
 
+            self.cuda_context = None
+            # Save current CUDA context to be pushed to threads later
+            try: 
+                import pycuda.driver as drv
+                self.cuda_context = drv.Context.get_current()
+            except:
+                pass
+
             self.units = self.dev.units
             self.quiet = device_options.quiet
             self.kernel_source = kernel_source
@@ -44,6 +53,32 @@ class ParallelRunner(Runner):
 
     def get_environment(self, tuning_options):
         return self.dev.get_environment()
+    
+    """
+
+    :param element: The current configuration inside the parameter space currently being observed
+    :type element: tuple
+
+    :param tuning_options: A dictionary with all options regarding the tuning process.
+    :type tuning_options: kernel_tuner.interface.Options
+
+    :returns: 
+    """
+    def single_compilation(self, element, tuning_options):
+        ## Copy cuda_context onto the thread
+        if (self.cuda_context is not None):
+            self.cuda_context.push()
+        try:
+            ## Build params dictionary
+            params = dict(zip(tuning_options.tune_params.keys(), element))
+
+            result, func, to, instance = self.dev.compile(self.kernel_source, self.gpu_args, params, self.kernel_options, tuning_options)
+        finally:
+            # Ensures removal of the cuda_context from the thread when compilation fails
+            if (self.cuda_context is not None):
+                self.cuda_context.pop()
+
+        return result, func, to, instance
 
     def run(self, parameter_space, tuning_options):
         """
@@ -53,68 +88,88 @@ class ParallelRunner(Runner):
 
         :param tuning_options: A dictionary with all options regarding the tuning
             process.
-        :type tuning_options: kernel_tuner.iterface.Options
+        :type tuning_options: kernel_tuner.interface.Options
 
         :returns: A list of dictionaries for executed kernel configurations and their
             execution times.
         :rtype: dict())
 
         """
-        logging.debug('sequential runner started for ' + self.kernel_options.kernel_name)
+        logging.debug('parallel runner started for ' + self.kernel_options.kernel_name)
 
         results = []
+        warmup_time = 0
 
-        # iterate over parameter space
-        for element in parameter_space:
-            params = dict(zip(tuning_options.tune_params.keys(), element))
+        # Results for the individual threads
+        future_results = {}
 
-            result = None
-            warmup_time = 0
+        # attempt to warmup the GPU by running the first config in the parameter space and ignoring the result
+        if not self.warmed_up:
+            first = next(iter(parameter_space))
+            params = dict(zip(tuning_options.tune_params.keys(), first))
+            warmup_time = perf_counter()
+            self.dev.compile(self.kernel_source, self.gpu_args, params, self.kernel_options, tuning_options)
+            self.warmed_up = True
+            warmup_time = 1e3 * (perf_counter() - warmup_time)
 
-            # check if configuration is in the cache
-            x_int = ",".join([str(i) for i in element])
-            if tuning_options.cache and x_int in tuning_options.cache:
-                params.update(tuning_options.cache[x_int])
-                params['compile_time'] = 0
-                params['verification_time'] = 0
-                params['benchmark_time'] = 0
-            else:
-                # attempt to warmup the GPU by running the first config in the parameter space and ignoring the result
-                if not self.warmed_up:
-                    warmup_time = perf_counter()
-                    self.dev.compile(self.kernel_source, self.gpu_args, params, self.kernel_options, tuning_options)
-                    self.warmed_up = True
-                    warmup_time = 1e3 * (perf_counter() - warmup_time)
+        # iterate over parameter space using thread pool
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as batch_executor:
 
-                result, func, to, instance = self.dev.compile(self.kernel_source, self.gpu_args, params, self.kernel_options, tuning_options)
+            # Queue all tasks
+            for element in parameter_space:
+                future_results[(batch_executor.submit(self.single_compilation, element, tuning_options))] = element
 
-                if func is not None:
-                    self.dev.benchmark_kernel(instance, func, self.gpu_args, to, result)
-                    
-                params.update(result)
+            # Choronologically collect results
+            for future_result in as_completed(future_results):
 
-                if tuning_options.objective in result and isinstance(result[tuning_options.objective], ErrorConfig):
-                    logging.debug('kernel configuration was skipped silently due to compile or runtime failure')
+                # Retrieve element back to map result to
+                element = future_results[future_result]
+                params = dict(zip(tuning_options.tune_params.keys(), element))
+                x_int = ",".join([str(i) for i in element])
+                result = None
 
-            # only compute metrics on configs that have not errored
-            if tuning_options.metrics and not isinstance(params.get(tuning_options.objective), ErrorConfig):
-                params = process_metrics(params, tuning_options.metrics)
+                # Check if result is already in the cache
+                if tuning_options.cache and x_int in tuning_options.cache:
+                    params.update(tuning_options.cache[x_int])
+                    params['compile_time'] = 0
+                    params['verification_time'] = 0
+                    params['benchmark_time'] = 0
+                else:
+                    # Retrieve the result
+                    result, func, to, instance = future_result.result()
 
-            # get the framework time by estimating based on other times
-            total_time = 1000 * ((perf_counter() - self.start_time) - warmup_time) 
-            params['strategy_time'] = self.last_strategy_time
-            params['framework_time'] = max(total_time - (params['compile_time'] + params['verification_time'] + params['benchmark_time'] + params['strategy_time']), 0)
-            params['timestamp'] = str(datetime.now(timezone.utc))
-            self.start_time = perf_counter()
+                    # Only benchmark if the compilation succeeded
+                    if func is not None:
+                        benchmark_result = self.dev.benchmark_kernel(instance, func, self.gpu_args, to, result)
 
-            if result:
-                # print configuration to the console
-                print_config_output(tuning_options.tune_params, params, self.quiet, tuning_options.metrics, self.units)
+                        # Save benchmark results
+                        result.update(benchmark_result)
 
-                # add configuration to cache
-                store_cache(x_int, params, tuning_options)
+                    # Map result to the parameter
+                    params.update(result)
 
-            # all visited configurations are added to results to provide a trace for optimization strategies
-            results.append(params)
+                    if tuning_options.objective in result and isinstance(result[tuning_options.objective], ErrorConfig):
+                        logging.debug('kernel configuration was skipped silently due to compile or runtime failure')
+
+                # only compute metrics on configs that have not errored
+                if tuning_options.metrics and not isinstance(params.get(tuning_options.objective), ErrorConfig):
+                    params = process_metrics(params, tuning_options.metrics)
+
+                # get the framework time by estimating based on other times
+                total_time = 1000 * ((perf_counter() - self.start_time) - warmup_time) 
+                params['strategy_time'] = self.last_strategy_time
+                params['framework_time'] = max(total_time - (params['compile_time'] + params['verification_time'] + params['benchmark_time'] + params['strategy_time']), 0)
+                params['timestamp'] = str(datetime.now(timezone.utc))
+                self.start_time = perf_counter()
+
+                if result:
+                    # print configuration to the console
+                    print_config_output(tuning_options.tune_params, params, self.quiet, tuning_options.metrics, self.units)
+
+                    # add configuration to cache
+                    store_cache(x_int, params, tuning_options)
+
+                # all visited configurations are added to results to provide a trace for optimization strategies
+                results.append(params)
 
         return results
